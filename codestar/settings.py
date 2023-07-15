267c4d7624f0bc405f7c4cf8f1d532fbf545ba7b
BjornRodin <bjornrodin@gmail.com>
2023-07-15 21:24:28 +0000

    Add allowed host for heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangoblog-walthrough-rodin.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['djangoblog-walthrough-rodin.herokuapp.com', 'localhost', 'djangoblog-walkthrough-rodin-4dca876d5b28.herokuapp.com']
 
 
 # Application definition
